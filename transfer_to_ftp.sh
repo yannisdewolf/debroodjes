b4815050b34bd9ec3108f23ea02916da6b408c87
yannis <yannisdewolf@gmail.com>
2019-01-27 13:07:43 +0100

    host = variable

--- transfer_to_ftp.sh
+++ transfer_to_ftp.sh
@@ -3,8 +3,8 @@
 LOCAL_SOURCES=./dist/debroodjes
 USERNAME=${ftp_user_name}
 PASSWORD=${ftp_password}
-HOST=185.182.57.5
-#HOST=$ftp_host${ftp_host}
+#HOST=185.182.57.5
+HOST=$ftp_host${ftp_host}
 
 echo "starting transfer to: ${HOST}"
 
