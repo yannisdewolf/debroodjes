4a9e09774501588831fe036befe77b6acc22267e
yannis <yannisdewolf@gmail.com>
2019-01-27 13:03:54 +0100

    extra logging

--- transfer_to_ftp.sh
+++ transfer_to_ftp.sh
@@ -6,11 +6,14 @@ PASSWORD=${ftp_password}
 HOST=185.182.57.5
 #HOST=$ftp_host${ftp_host}
 
-echo "starting transfer to: " ${HOST}
+echo "starting transfer to: ${HOST}"
 
 #################################
 ##### delete existing files #####
 #################################
+
+echo "start deleting files..."
+
 for f in `curl -l ftp://${USERNAME}:${PASSWORD}@${HOST}/domains/flinkpoyd.be/public_html/debroodjes/backup/`; do
 
   # Delete each file individually
@@ -19,15 +22,21 @@ for f in `curl -l ftp://${USERNAME}:${PASSWORD}@${HOST}/domains/flinkpoyd.be/pub
   echo "deleted $f"
 done
 
+echo "files remotely deleted"
+
 ############################
 ##### upload new files #####
 ############################
+
+echo "start uploading files"
+
 for fileToUpload in `find ${LOCAL_SOURCES} -maxdepth 1 -type f`; do
 
   echo ${fileToUpload}
   curl -T ${fileToUpload} ftp://${USERNAME}:${PASSWORD}@${HOST}/domains/flinkpoyd.be/public_html/debroodjes/backup/
 
 done
+echo "upload files done"
 
 
 #curl -T ../dist/debroodjes/assets/.htaccess ftp://${USERNAME}:${PASSWORD}@${HOST}/domains/flinkpoyd.be/public_html/debroodjes/backup/
