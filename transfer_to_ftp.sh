139c21a3d131e3dcfacc4bff23f69aa3dee7d5df
yannis <yannisdewolf@gmail.com>
2019-01-27 11:01:23 +0100

    deploy?

--- transfer_to_ftp.sh
+++ transfer_to_ftp.sh
@@ -10,29 +10,24 @@ echo "starting transfer to: " ${HOST}
 #################################
 ##### delete existing files #####
 #################################
-echo "start deleting files"
 for f in `curl -l ftp://${USERNAME}:${PASSWORD}@${HOST}/domains/flinkpoyd.be/public_html/debroodjes/backup/`; do
 
   # Delete each file individually
   echo "deleting $f ..."
-  curl ftp://${USERNAME}:${PASSWORD}@${HOST}/domains/flinkpoyd.be/public_html/debroodjes/backup/ -Q "DELE domains/flinkpoyd.be/public_html/debroodjes/backup/$f"
+  curl -v ftp://${USERNAME}:${PASSWORD}@${HOST}/domains/flinkpoyd.be/public_html/debroodjes/backup/ -Q "DELE domains/flinkpoyd.be/public_html/debroodjes/backup/$f"
   echo "deleted $f"
 done
 
-echo "all files deleted"
-
-echo "start uploading files"
 ############################
 ##### upload new files #####
 ############################
-for fileToUpload in `find ./dist/debroodjes -maxdepth 1 -type f`; do
+for fileToUpload in `find ${LOCAL_SOURCES} -maxdepth 1 -type f`; do
 
-#  echo $fileToUpload
+  echo ${fileToUpload}
   curl -T ${fileToUpload} ftp://${USERNAME}:${PASSWORD}@${HOST}/domains/flinkpoyd.be/public_html/debroodjes/backup/
 
 done
 
-echo "files uploaded"
 
-curl -T ./dist/debroodjes/assets/.htaccess ftp://${USERNAME}:${PASSWORD}@${HOST}/domains/flinkpoyd.be/public_html/debroodjes/backup/
+#curl -T ../dist/debroodjes/assets/.htaccess ftp://${USERNAME}:${PASSWORD}@${HOST}/domains/flinkpoyd.be/public_html/debroodjes/backup/
 
